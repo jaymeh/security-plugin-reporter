e5141886e59315c447f60fd44178210f07cea50b
Jamie Sykes <jaymeh@users.noreply.github.com>
2023-03-24 22:51:58 +0000

    Initial commit.

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+# Release Notes for security-header-reporter
+
+## 1.0.0
+- Initial release
---
+++ src/models/Settings.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace jaymeh\craftsecurityheaderreporter\models;
+
+use Craft;
+use craft\base\Model;
+
+/**
+ * security-header-reporter settings
+ */
+class Settings extends Model
+{
+}
